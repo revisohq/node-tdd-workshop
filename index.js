76e7c8dc18e10e8e324acb64f13b412b26bd5447
marco pegoraro <marco.pegoraro@gmail.com>
2021-02-26 17:12:31 +0100

    Add specific error handling

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+
+class FirstArgumentTypeError extends Error {}
+class SecondArgumentTypeError extends Error {}
+
 const sum = (a, b) => {
-  if((typeof a) !== 'number'){
-    throw new Error('Value a is not a number!')
+  if ((typeof a) !== 'number'){
+    throw new FirstArgumentTypeError('Value a is not a number!')
   }
+
+  if((typeof b) !== 'number'){
+    throw new SecondArgumentTypeError('Value b is not a number!')
+  }
+
   return a+b;
 };
 
 module.exports = {
   sum,
+  FirstArgumentTypeError,
+  SecondArgumentTypeError
 };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { sum } = require("./index");
+const { sum, FirstArgumentTypeError, SecondArgumentTypeError } = require("./index");
 
 describe("some meaningful tests", () => {
   test("it should sum 1 and 2", () => {
@@ -11,7 +11,7 @@ describe("some meaningful tests", () => {
     expect(result).toBe(7);
   });
 
-  test("it should throw an error in of string", () => {
+  test("it should throw an error if first parameter is a string", () => {
     const shouldNotWork = jest.fn(() => {
       //console.log('I should NOT see this')
     });
@@ -25,6 +25,28 @@ describe("some meaningful tests", () => {
       shouldNotWork();
     } catch (err) {
       shouldWork(err);
+      expect(err).toBeInstanceOf(FirstArgumentTypeError)
+    }
+
+    expect(shouldWork.mock.calls.length).toBe(1)
+    expect(shouldNotWork.mock.calls.length).toBe(0)
+  });
+
+  test("it should throw an error if second parameter is a string", () => {
+    const shouldNotWork = jest.fn(() => {
+      //console.log('I should NOT see this')
+    });
+
+    const shouldWork = jest.fn((err) => {
+      //console.log('it works', err)
+    })
+
+    try {
+      sum(3, '4');
+      shouldNotWork();
+    } catch (err) {
+      shouldWork(err);
+      expect(err).toBeInstanceOf(SecondArgumentTypeError)
     }
 
     expect(shouldWork.mock.calls.length).toBe(1)
