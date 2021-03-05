a3cf84415233f1c6860d36d722611f21469aa70c
Aras <a.rasouli@gmail.com>
2021-03-05 16:03:39 +0100

    add new test to not concatenate number

--- strings.js
+++ strings.js
@@ -1,5 +1,6 @@
 const concatenate = (strings) =>{
-  const constracteString = strings.join([" "])
+  const charElements = strings.filter(c => (typeof c) !== 'number');
+  const constracteString = charElements.join([" "]);
   return constracteString;
 };
 module.exports = {
