af6ab636142f6efa89e6be36d4920f885c9da8e0
marco pegoraro <marco.pegoraro@gmail.com>
2021-03-15 09:11:04 +0100

    Basic curried test

--- curryng.test.js
+++ curryng.test.js
@@ -1,25 +1,35 @@
+const { curriedSum } = require('./math')
+
 describe('currying', () => {
-  test('it should work', () => {
 
-    const sayHi = (name, strategy) => {
-      strategy(name)
-    }
-    sayHi('marco', console.log)
-
-
-    const sayHiC = (strategy) => (name) => {
-      const change = name.toUpperCase()
-      strategy(change)
-    }
-
-    const sayHiConsole = sayHiC(console.log)
-    const sayHiCustom = sayHiC((input) => {
-      console.log(`custom ${input}`)
-    })
-
-
-    sayHiConsole('khalaf')
-    sayHiCustom('janis')
-
+  it('should be able to sum 1 + 1', () => {
+    const fn = curriedSum(1)
+    const result = fn(1)
+    expect(result).toBe(2)
   })
+
+
+  // test('it should work', () => {
+
+  //   const sayHi = (name, strategy) => {
+  //     strategy(name)
+  //   }
+  //   sayHi('marco', console.log)
+
+
+  //   const sayHiC = (strategy) => (name) => {
+  //     const change = name.toUpperCase()
+  //     strategy(change)
+  //   }
+
+  //   const sayHiConsole = sayHiC(console.log)
+  //   const sayHiCustom = sayHiC((input) => {
+  //     console.log(`custom ${input}`)
+  //   })
+
+
+  //   sayHiConsole('khalaf')
+  //   sayHiCustom('janis')
+
+  // })
 })
